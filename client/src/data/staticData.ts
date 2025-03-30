84cfc9c46b7d7ba4e4557d3b4c2dde97f8c48d71
Pruthviraj Dodiya <pruthvirajdodiya@Pruthvirajs-MacBook-Air.local>
2025-03-31 01:35:08 +0530

    deployment support

---
+++ client/src/data/staticData.ts
@@ -0,0 +1,8 @@
+// Copy the initial recipe data from your MemStorage initialization
+export const staticRecipes = [
+    // Copy recipe data from server/storage.ts initializeSampleData() function
+  ];
+  
+  export const staticCategories = [
+    // Copy category data from server/storage.ts
+  ];
